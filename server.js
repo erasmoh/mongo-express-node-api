9eee25ecfad4d0e56cb30ca7bd5f1ff2c38d8087
Erasmo Hernández <erasmoh@gmail.com>
2021-09-30 01:34:10 -0300

    Feat: Docker added to the node api

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const mongoose = require("mongoose");
 const bodyParse = require("body-parser");
 require("dotenv/config");
@@ -13,12 +13,13 @@ const postsRoute = require("./routes/posts");
 app.use("/posts", postsRoute);
 
 app.get("/", (req, res) => {
-  res.send("We are on home");
+    // res.send("We are on home");
+    res.json({ message: "Docker is easy" });
 });
 
 // DB connect
 mongoose.connect(process.env.DB_CONNECTION, () =>
-  console.log("Connected to DB")
+    console.log("Connected to DB")
 );
 
-app.listen(PORT, () => console.log(`it's alive on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`));
